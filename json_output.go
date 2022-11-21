6f953fb089889dc232f9b561de64a7ee0691e0f8
Connor Dufault <dufaultconnor@gmail.com>
2022-11-21 06:37:46 -0600

    output shape data to json files

---
+++ json_output.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func exportShapesToJson(shapes []Shape, destination string) {
+
+	fmt.Printf("Writing shape data files to %s...\n", destination)
+
+	// Get the absolute destination path
+	destination, err := filepath.Abs(destination)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := os.MkdirAll(destination, os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	for _, shape := range shapes {
+		shapeJson, err := json.Marshal(shape)
+
+		if err != nil {
+			panic(err)
+		}
+
+		filePath := filepath.Join(destination, fmt.Sprintf("%d.json", shape.RouteId))
+		writeToFile(shapeJson, filePath)
+	}
+}
+
+func writeToFile(contents []byte, path string) {
+	fmt.Printf("  %s\n", path)
+	if err := os.WriteFile(path, contents, 0666); err != nil {
+		panic(err)
+	}
+}
